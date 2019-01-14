3e5c4964f15703b4a7ff10188171e1ffe527c4b3
Daniel Wentsch <hello@wentsch.me>
2019-01-14 19:57:23 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = function ({ addVariant }) {
+    addVariant('first-child', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+            return `.first-child${separator}${className} >*:first-child`;
+        });
+    });
+};
